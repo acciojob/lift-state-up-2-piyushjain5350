af7b0087a55c71122537a9ab2d13adf2d3596672
Piyush Jain <piyushjain5350@gmail.com>
2023-08-14 15:05:22 +0530

    1. solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 
-import React from "react";
+import React ,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[input,setInput]=useState('')
+  function handlingFn(value){
+        setInput(value);
+  };
   return (
-    <div>
+    <div style={{backgroundColor:'green',padding:'10px'}}>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+        <div className="textContainer">{input}</div>
+        <Child handlingFn={handlingFn}/>
     </div>
   )
 }
 
+const Child =({handlingFn})=>{
+  function valueInput(event){
+    handlingFn(event.target.value);
+  }
+  return(
+    <div style={{backgroundColor:'darkorange',padding:'5px'}}>
+      <h1>Child Component</h1>
+      <input type="text" placeholder="Write Something" onChange={()=>{valueInput(event);}}/>
+    </div> 
+  )
+}
 export default App
